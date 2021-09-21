12bf5ef0482b4d625973ec8d69d6aa3dc5d00521
Akhmetjyuneid Turan <63200819+Cuneytturan@users.noreply.github.com>
2021-09-21 14:36:51 +0600

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,44 @@
+function big(element) {
+	element.style.fontSize = "30px";
+}
+
+function small(element){
+	element.style.fontSize = "25px";
+}
+
+function load(){
+	$(".text").animate({opacity: '1'}, "slow");
+}
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
